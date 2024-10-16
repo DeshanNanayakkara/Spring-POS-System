05624c58f76b1671937a42efe1e4027bb2172bb9
deshan <udayangadeshan2528@gmail.com>
2024-10-16 11:06:51 +0530

    AppInitializer extends from AbstractAnnotationConfigDispatcherServletInitializer

--- src/main/java/lk/ijse/gde68/springpossystem/AppInitializer.java
+++ src/main/java/lk/ijse/gde68/springpossystem/AppInitializer.java
@@ -1,4 +1,21 @@
 package lk.ijse.gde68.springpossystem;
 
-public class AppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
